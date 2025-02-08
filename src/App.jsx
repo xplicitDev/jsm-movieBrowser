992c49447ac2791f888c97b6c45e7268fb501683
xplicitDev <masoodforwork@gmail.com>
2025-02-08 23:59:06 +0530

    lastMinuteAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./components/search.jsx";
 import "./App.css";
 
+const API_BASE_URL = "http://www.omdbapi.com/?";
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async () => {
+    try {
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+    }
+  };
+
+  useEffect(() => {}, []);
   return (
     <main>
       <div className="pattern" />
