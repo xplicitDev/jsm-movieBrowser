84c79ef2c203852feb17de4bb5c6cea3c1a50ffa
xplicitDev <masoodforwork@gmail.com>
2025-02-11 00:49:33 +0530

    apiSorted&spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Search from "./components/search.jsx";
+import Spinner from "./components/spinner.jsx";
 import "./App.css";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
@@ -23,7 +24,7 @@ const App = () => {
     setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/disocver/movie?sort_by=popularity.desc`;
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -31,6 +32,7 @@ const App = () => {
       }
 
       const data = await response.json();
+      console.log(data);
 
       if (data.response == "False") {
         setErrorMessage(
@@ -66,16 +68,18 @@ const App = () => {
         </header>
 
         <section className="allMovies">
-          <h2>All Movies</h2>
+          <h2 className="mt-[40px]">All Movies</h2>
 
           {isLoading ? (
-            <p className="text-white">Loading....</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className="text-red-600">{errorMessage}</p>
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <p className="text-white">{movie.title}</p>
+                <p key={movie.id} className="text-white">
+                  {movie.title}
+                </p>
               ))}
             </ul>
           )}
