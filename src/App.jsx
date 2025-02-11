dfde97fed72f40bada87a019ea583b19e14344cc
xplicitDev <masoodforwork@gmail.com>
2025-02-11 14:08:11 +0530

    chatgpt left

--- src/App.jsx
+++ src/App.jsx
@@ -68,7 +68,7 @@ const App = () => {
         </header>
 
         <section className="all-movies">
-          <h2 className="mt-[40px]">All Movies</h2>
+          <h2 className="mt-[40px] text-left">All Movies</h2>
 
           {isLoading ? (
             <Spinner />
