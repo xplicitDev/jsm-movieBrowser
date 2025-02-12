ec374dc4fd28e6d0d07b8f38d697fa3ce2ca8be4
xplicitDev <masoodforwork@gmail.com>
2025-02-12 23:47:52 +0530

    enhance movie fetching with search functionality and update poster fallback image

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,14 @@ const App = () => {
   const [movieList, setmovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = "") => {
     setIsLoading(true);
     setErrorMessage("");
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if (!response.ok) {
@@ -51,8 +53,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
   return (
     <main>
       <div className="pattern" />
--- src/components/movieCard.jsx
+++ src/components/movieCard.jsx
@@ -9,7 +9,7 @@ const movieCard = ({
         src={
           poster_path
             ? `https://image.tmdb.org/t/p/w500${poster_path}`
-            : "/no-movie.png"
+            : "/No-Poster.png"
         }
         alt={title}
       />
@@ -23,6 +23,12 @@ const movieCard = ({
           </div>
 
           <span>•</span>
+          <p className="lang">{original_language}</p>
+
+          <span>•</span>
+          <p className="year">
+            {release_date ? release_date.split("-")[0] : "N/A"}
+          </p>
         </div>
       </div>
     </div>
